1b164ee14e4fdce7fa022ed810c805edb59f1225
FRXN-ZZ <edmuelagu@uide.edu.ec>
2025-12-04 13:18:56 +0000

    Mi segundo comit

---
+++ Script2.sh
@@ -0,0 +1,8 @@
+#uname -a muestra información detallada del sistema operativo.
+uname -a
+#Muestra información del procesador (CPU).
+lscpu
+#Muestra la memoria disponible y usada (RAM y swap). La opción -h significa “human-readable”, es decir, muestra los valores en MB o GB.
+free -h
+#Muestra el uso del espacio en disco.
+df -h
